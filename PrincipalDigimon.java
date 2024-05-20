628859257d750ea7d212cbc3663113ca63b9c4f1
patriciaaguayo <patriciaaguayop@gmail.com>
2024-05-21 00:58:13 +0200

    Prueba 2 de subida

--- PrincipalDigimon.java
+++ PrincipalDigimon.java
@@ -3,7 +3,7 @@ public class PrincipalDigimon {
     public static void main(String[] args) {
 
         System.out.println("\n Es hora de cazar Digimons");
-
+        System.out.println("\n Hola");
         int edad;
     }
 }
