4b427792b88691acfcd51d98f0533c5ff89a1b81
patriciaaguayo <patriciaaguayop@gmail.com>
2024-05-21 13:55:24 +0200

    Realizado lo básico de la clase Digimon

--- Digimon.java
+++ Digimon.java
@@ -1,17 +1,25 @@
 public class Digimon {
 
     private String NombreDigimon;
+    private int Nivel;
     private int PuntosAtaque;
-    private String Salud;
-    private int CantidadAtaques;
-    private int DP1; // Ataque 1
-    private int DP2; // Ataque 2
+    private int Salud;
+    private int DP1; // Ataque 1 Común Gasta 1 cada vez que se usa
+    private int DP2; // Ataque 2 Especial Gasta 2 cada vez que se usa
 
     // Constructor
 
-    public Digimon(){
+    public Digimon(String nombre){
 
+        this.NombreDigimon = nombre;
+        this.Nivel = (int)(Math.random()*5)+1;
+        this.PuntosAtaque = this.Nivel*5;
+        this.Salud = this.Nivel*10;
+        this.DP1 = 10;
+        this.DP2 = 10;
     }
+
+    // SETTERS Y GETTERS
 
     public void setNombreDigimon(String nombreDigimon) {
         NombreDigimon = nombreDigimon;
@@ -21,44 +29,44 @@ public class Digimon {
         return NombreDigimon;
     }
 
-    public void setPuntosAtaque(int puntosAtaque) {
-        PuntosAtaque = puntosAtaque;
+    public int getNivel(){
+        return Nivel;
     }
 
     public int getPuntosAtaque() {
         return PuntosAtaque;
     }
 
-    public void setSalud(String salud) {
-        Salud = salud;
-    }
-
-    public String getSalud() {
+    public int getSalud() {
         return Salud;
-    }
-
-    public void setCantidadAtaques(int cantidadAtaques) {
-        CantidadAtaques = cantidadAtaques;
-    }
-
-    public int getCantidadAtaques() {
-        return CantidadAtaques;
-    }
-
-    public void setDP1(int DP1) {
-        this.DP1 = DP1;
     }
 
     public int getDP1() {
         return DP1;
     }
 
-    public void setDP2(int DP2) {
-        this.DP2 = DP2;
-    }
-
     public int getDP2() {
         return DP2;
+    }
+
+    // MÉTODOS
+
+    public void Ataque1(Digimon enemigo) {
+        enemigo.Salud -= this.PuntosAtaque;
+        this.DP1--;
+
+        System.out.println("\n " + this.NombreDigimon + " ha usado Ataque1 contra "
+        + enemigo.NombreDigimon + " y le ha quitado " + this.Salud +
+        " puntos de salud");
+    }
+
+    public void Ataque2(Digimon enemigo) {
+        enemigo.Salud -= this.PuntosAtaque*2;
+        this.DP2 -= 2;
+
+        System.out.println("\n " + this.NombreDigimon + " ha usado Ataque2 contra "
+                + enemigo.NombreDigimon + " y le ha quitado " + this.Salud +
+                " puntos de salud");
     }
 
 }
