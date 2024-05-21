a8617e934c1d2fc36b7775eaa1005d5f9e68e696
Patri <patriciaaguayop@gmail.com>
2024-05-21 16:08:48 +0200

    Hecha estructura básica de la clase Domador

--- Domador.java
+++ Domador.java
@@ -1,4 +1,52 @@
 public class Domador {
 
+    // Atributos
 
+    private int Contador = 0;
+    private Digimon[] Equipo = new Digimon[3];
+    private String NombreDomador;
+
+    // Constructor
+
+    public Domador(String nombre){
+
+        this.NombreDomador = nombre;
+        this.Equipo[Contador++] = new Digimon("Aleatorio");
+    }
+
+    // SETTERS Y GETTERS
+
+    public void setNombreDomador(String nombreDomador) {
+        NombreDomador = nombreDomador;
+    }
+
+    public String getNombreDomador() {
+        return NombreDomador;
+    }
+
+    // MÉTODOS
+
+    public void captura(Digimon digimon) {
+        if (digimon.getSalud() <= digimon.getNivel() * 10 - 20) {
+            if (Contador < 3) {
+                Equipo[Contador++] = digimon;
+                System.out.println("\n "+ digimon.getNombreDigimon() + " se ha unido a tu equipo.");
+            } else {
+                System.out.println("\n Equipo esta lleno. No puedes capturar mas Digimons.");
+            }
+        } else {
+            System.out.println("\n "+ digimon.getNombreDigimon() + " no se puede unir. Su salud sigue siendo muy alta.");
+        }
+    }
+
+    public void VerEquipo(Digimon[]equipo){
+
+        String miEquipo = "";
+
+        for(int i = 0; i<3; i++){
+            miEquipo += "\n " + equipo[i].getNombreDigimon();
+
+        }
+        System.out.println(miEquipo);
+    }
 }
